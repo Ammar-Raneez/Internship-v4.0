9686a92f6027c3593aed55cea195752523a8c974
ammar <ammarraneez@gmail.com>
2021-10-04 15:39:01 +0530

    feat: add exercise OOP version

---
+++ warmup-exercise-oop/src/ArrayAdder.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class ArrayAdder {
+    private int[] firstArray;
+    private int[] secondArray;
+
+    public void setFirstArray(int[] arr) {
+        this.firstArray = arr;
+    }
+
+    public void setSecondArray(int[] arr) {
+        this.secondArray = arr;
+    }
+
+    public int[] getNewArray() throws Exception {
+        return Helper.calculateTotal(this.firstArray, this.secondArray);
+    }
+
+    public static void main(String[] args) throws Exception {
+        ArrayAdder arrayAdder = new ArrayAdder();
+        arrayAdder.setFirstArray(Helper.getInput());
+        arrayAdder.setSecondArray(Helper.getInput());
+
+        try {
+            int[] totalArr = arrayAdder.getNewArray();
+            System.out.println("Total: " + Arrays.toString(totalArr));
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+}
---
+++ warmup-exercise-oop/src/Helper.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class Helper {
+    static Scanner sc = new Scanner(System.in);
+
+    static int[] getInput() {
+        while (true) {
+            try {
+                System.out.println("Enter numbers, separated by commas: ");
+                String[] numbers = sc.nextLine().split(", ");
+                int[] arr = new int[numbers.length];
+                for (int i = 0; i < numbers.length; i++) {
+                    arr[i] = Integer.parseInt(numbers[i]);
+                }
+
+                return arr;
+            } catch (Exception e) {
+                System.out.println("Invalid input, Please try again");
+            }
+        }
+    }
+
+    static int[] calculateTotal(int[] firstArray, int[] secondArray) throws Exception {
+        try {
+            validateLength(firstArray, secondArray);
+            int[] totalArray = new int[firstArray.length];
+            for (int i = 0; i < firstArray.length; i++) {
+                totalArray[i] = firstArray[i] + secondArray[i];
+            }
+
+            return totalArray;
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
+
+    private static void validateLength(int[] firstArray, int[] secondArray) throws Exception {
+        if (firstArray.length != secondArray.length) {
+            throw new Exception("Inputs were different in length");
+        }
+    }
+}
